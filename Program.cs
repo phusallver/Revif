6080c7f219e06c3fd97875fda276cd52c9cc47e6
rasyeeed <muhammadnurrasyid@gmail.com>
2023-05-07 22:57:35 +0700

    class login (belom selese)

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
-﻿internal class Program
+﻿using Revif;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Login ras = new Login();
+        ras.ReadJSON();
     }
-}
+}   
