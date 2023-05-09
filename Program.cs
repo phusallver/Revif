2512a8fdc4cbe71735fa3cf30dd68287e47962fd
KuRynnn <fahryan107@gmail.com>
2023-05-09 22:44:45 +0700

    Revert "Ini bisa jadi final"
    This reverts commit f149ed5b46db019cc65a02a19f067deafda6b8c7.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,7 @@
-﻿using Revif;
-
-internal class Program
+﻿internal class Program
 {
     private static void Main(string[] args)
     {
-        UnggahJasaConfig config = new UnggahJasaConfig();
-        //config.tambahData();
-        Console.WriteLine("-------------MENU-------------");
-        Console.WriteLine("1. Tambah data jasa");
-        Console.WriteLine("2. Show data pengguna beserta jasa");
-        Console.WriteLine("3. EXIT");
-        config.menu();  
+        Console.WriteLine("Hello, World!");
     }
 }
