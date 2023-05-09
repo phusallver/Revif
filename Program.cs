c1e774432465a6a1c354fde5170e3b92fa77f158
KuRynnn <fahryan107@gmail.com>
2023-05-09 22:49:47 +0700

    Ini keknya Final

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
-﻿internal class Program
+﻿using Revif;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        UnggahJasaConfig config = new UnggahJasaConfig();
+        //config.tambahData();
+        Console.WriteLine("-------------MENU-------------");
+        Console.WriteLine("1. Tambah data jasa");
+        Console.WriteLine("2. Show data pengguna beserta jasa");
+        Console.WriteLine("3. EXIT");
+        config.menu();
     }
 }
