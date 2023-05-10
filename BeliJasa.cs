a7c7800f15b7222d56b47cd2466f6e8558f4693a
Drakkura <gonhgyhjj@student.telkomuniversity.ac.id>
2023-05-10 11:58:31 +0700

    Assalamualaikum

---
+++ BeliJasa.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revif
+{
+    public class BeliJasa
+    {
+        public BeliJasa() { }
+        public Pesanan bikinPesanan(Akun akun, string nama_pesanan,
+            string jenis_jasa, string deskripsi_pesanan)
+        {
+            Pesanan pesanan = new Pesanan();
+            pesanan.akunPenyedia = akun;
+            pesanan.nama_pesanan = nama_pesanan;
+            pesanan.jenis_jasa = jenis_jasa;
+            pesanan.deskripsi_pesanan = deskripsi_pesanan;
+            pesanan.status_pesanan = "Belum terbayar";
+            return pesanan;
+        }
+        public Pesanan tambahPesanan(List<Akun> lis)
+        {
+            Pesanan data;
+            Akun akun = cariAkun(lis);
+            Console.Write("Enter Order Name : ");
+            string nama_pesanan = Console.ReadLine();
+            Console.Write("Enter Jenis Order : ");
+            string jenis_jasa = Console.ReadLine();
+            Console.Write("Enter Order Description : ");
+            string deskripsi_pesanan = Console.ReadLine();
+            data = bikinPesanan(akun, nama_pesanan, jenis_jasa, deskripsi_pesanan);
+            return data;
+        }
+
+        geser.geser gsr = new geser.geser();
+
+        public Akun cariAkun(List<Akun> lis)
+        {
+            Console.Write("Enter Penyedia Jasa : ");
+            string cariAkun = Console.ReadLine();
+            Akun akun = new Akun("", ""); int i;
+            while (akun.username != cariAkun)
+            {
+                i = 0;
+                while (gsr.geserr<Akun>(lis, i))
+                {
+                    if (lis[i].username == cariAkun)
+                    {
+                        akun = lis[i];
+                    }
+                    i++;
+                }
+                if (akun.username == "")
+                {
+                    Console.WriteLine("Username not found.");
+                    Console.Write("Enter Penyedia Jasa : ");
+                    cariAkun = Console.ReadLine();
+                }
+            }
+            return akun;
+        }
+        public void tambahDataPesanan(Akun akun, List<Akun> lis)
+        {
+            Pesanan pesanan = tambahPesanan(lis);
+            akun.pembeli.pemesanan.Add(pesanan);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,44 @@
-﻿internal class Program
+﻿using Revif;
+using System.Security.Cryptography.X509Certificates;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        LoginConvig loginConvig = new LoginConvig();
+        bool login = false;
+        Akun akunMasuk;
+        Console.WriteLine("Silahkan log in...");
+        while (!login)
+        {
+            Console.Write("Username : ");
+            string username = Console.ReadLine();
+            Console.Write("Password : ");
+            string password = Console.ReadLine();
+            foreach (Akun akun in loginConvig.ListAkun.akun)
+            {
+                if (akun.username == username && akun.password == password)
+                {
+                    akunMasuk = akun;
+                    login = true;
+                }
+
+            }
+            if (!login)
+            {
+                Console.WriteLine("Username atau password salah, silahkan masukkan kembali...");
+            }
+        }
+        EditProfile edit = new EditProfile(loginConvig.ListAkun.akun[1]);
+        Console.WriteLine("Username sebelum : " + loginConvig.ListAkun.akun[1].username);
+        edit.username();
+        Console.WriteLine("Username sesudah : " + loginConvig.ListAkun.akun[1].username);
+        /*
+        BeliJasa beliJasa = new BeliJasa();
+        beliJasa.tambahDataPesanan(loginConvig.ListAkun.akun[1], loginConvig.ListAkun.akun);
+        Console.WriteLine(loginConvig.ListAkun.akun[1].pembeli.pemesanan[0].deskripsi_pesanan);
+
+        Menu menu = new Menu();
+        menu.masuk();*/
     }
 }
